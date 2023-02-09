56616df52e36c1bf340498cfff33b3cf1c1aed01
adilorenzodema <a.dilorenzo@dema-engineering.it>
2023-02-09 12:06:50 +0100

    filer on edit area

--- src/app/components/area-management/edit-area/edit-area.component.ts
+++ src/app/components/area-management/edit-area/edit-area.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import * as moment from 'moment';
 import { Area } from 'src/app/domain/class';
@@ -12,11 +14,13 @@ import { AreaManagementService } from 'src/app/service/area-management.service';
   styleUrls: ['./edit-area.component.css']
 })
 export class EditAreaComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   public area: Area;
   public formGroup!: FormGroup;
-  public users: UserAssociated[] = [];
+  public dataSource = new MatTableDataSource<UserAssociated>;
+  public displayedColumns = ['firstName', 'granted'];
   public grantedUsers: UserAssociated[] = [];
-  public selectedUsers: UserAssociated[] = [];
+  public filterGrantedUsers: UserAssociated[] = [];
   public viewModeUser = true;
 
   constructor(
@@ -31,18 +35,38 @@ export class EditAreaComponent implements OnInit {
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
       ctrlAreaName: [this.area.areaName, Validators.required],
-      ctrlCreationDate: [moment(this.area.creationDate).format('DD/mm/YYYY')]
+      ctrlCreationDate: [moment(this.area.creationDate).format('DD/mm/YYYY')],
+      ctrlSearch: ['']
     });
     this.apiGetAssociateUserArea();
+  }
+
+  public filter(): void {
+    const filterValue = this.formGroup.get('ctrlSearch')?.value;
+    if (this.viewModeUser) {
+      if (filterValue.length === 0) {
+        this.filterGrantedUsers = [...this.grantedUsers];
+      } else {
+        this.filterGrantedUsers = [...this.filterGrantedUsers.filter(x => x.firstName.toLowerCase().indexOf(filterValue) >= 0)];
+      }
+    } else {
+      this.dataSource.filter = filterValue.trim().toLowerCase();
+    }
+  }
+
+  public saveAssociation(): void {
+    console.log(this.dataSource.data)
   }
 
   private apiGetAssociateUserArea(): void {
     this.areaManageService.getAssociateUserArea(this.area.idArea).subscribe(
       (users) => (
-        this.users = users,
+        this.dataSource.data = users,
+        this.dataSource.paginator = this.paginator,
         users.forEach(
           (user) => { if (user.granted) this.grantedUsers.push(user); }
-        )
+        ),
+        this.filterGrantedUsers = [...this.grantedUsers]
       )
     );
   }
