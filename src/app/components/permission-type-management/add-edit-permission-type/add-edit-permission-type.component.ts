7620e019c026928fa97b4a57fd0c1d0c95e861e7
adilorenzodema <a.dilorenzo@dema-engineering.it>
2023-02-14 14:18:16 +0100

    add permission type

--- src/app/components/permission-type-management/add-edit-permission-type/add-edit-permission-type.component.ts
+++ src/app/components/permission-type-management/add-edit-permission-type/add-edit-permission-type.component.ts
@@ -1,20 +1,78 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { SnackBar } from 'dema-movyon-template';
+import { AddTypePermission } from 'src/app/domain/class';
+import { PermissionTypeManagementService } from 'src/app/service/permission-type-management.service';
 
 @Component({
   selector: 'app-add-edit-permission-type',
   templateUrl: './add-edit-permission-type.component.html',
-  styles: [
+  styles: [`
+  .timeSlot {
+    width: 60%;
+    border: 2px solid #49beaa;
+    border-radius: 10px;
+    padding: 2%;
+    margin: 1%;
+  }
+  `
   ]
 })
 export class AddEditPermissionTypeComponent implements OnInit {
   public formGroup: FormGroup;
+  public days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+  public holidays = ['holiday', 'preHoliday'];
+  public complete = true;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private snackBar: SnackBar,
+    private permissionTypeService: PermissionTypeManagementService
+  ) { }
+
+  get timesSlot(): FormArray {
+    return this.formGroup.get('ctrlTimesSlot') as FormArray;
+  }
 
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
-      ctrlName: ['', Validators.required]
+      ctrlName: ['', Validators.required],
+      ctrlTimesSlot: this.formBuilder.array([this.buildTimeSlotArray()]),
+    });
+  }
+
+  public addPermissionType(): void {
+    this.complete = false;
+    const name = this.formGroup.get('ctrlName').value;
+    const list = this.formGroup.get('ctrlTimesSlot').value;
+    const newPermissionType = new AddTypePermission(name, list);
+    this.permissionTypeService.addPermissionType(newPermissionType).subscribe({
+      error: () => this.complete = true,
+      complete: () => (this.snackBar.showMessage('tipo permesso inserito correttamente', 'INFO') , this.complete = true)
+    });
+  }
+
+  public addTimeSlot(): void {
+    this.timesSlot.push(this.buildTimeSlotArray());
+  }
+
+  public removeTimeSlot(index: number): void {
+    this.timesSlot.removeAt(index);
+  }
+
+  public buildTimeSlotArray(): FormGroup {
+    return this.formBuilder.group({
+      startTime: ['', Validators.required],
+      endTime: ['', Validators.required],
+      monday: [false],
+      tuesday: [false],
+      wednesday: [false],
+      thursday: [false],
+      friday: [false],
+      saturday: [false],
+      sunday: [false],
+      holiday: [false],
+      preHoliday: [false]
     });
   }
 
--- src/app/service/permission-type-management.service.ts
+++ src/app/service/permission-type-management.service.ts
@@ -3,6 +3,7 @@ import { Inject, Injectable } from '@angular/core';
 import { HttpUtils } from 'dema-movyon-template';
 import { CookieService } from 'ngx-cookie-service';
 import { catchError, Observable } from 'rxjs';
+import { AddTypePermission } from '../domain/class';
 import { PermissionType } from '../domain/interface';
 import { Cookie } from '../shared/utils/cookieClass';
 
@@ -24,6 +25,15 @@ export class PermissionTypeManagementService {
       params: HttpUtils.createHttpParams({ token: Cookie.getToken(this.cookieService), name: keyword, active: isActive })
     };
     return this.http.get<PermissionType[]>(this.apiURL + '/getAllPermissionTypes', options)
+      .pipe(catchError(err => { throw err; }));
+  }
+
+  addPermissionType(permissionType: AddTypePermission): Observable<void> {
+    const options = {
+      headers: new HttpHeaders().set('Content-Type', 'application/json'),
+      params: HttpUtils.createHttpParams({ token: Cookie.getToken(this.cookieService) })
+    };
+    return this.http.post<void>(this.apiURL + '/addPermissionType', permissionType, options)
       .pipe(catchError(err => { throw err; }));
   }
 }
