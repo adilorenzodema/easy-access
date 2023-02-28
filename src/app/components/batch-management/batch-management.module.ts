99aac66288279bf8ac2ec6411f8b37755c277172
adilorenzodema <a.dilorenzo@dema-engineering.it>
2023-02-28 16:25:55 +0100

    module batch

--- src/app/components/batch-management/batch-management.module.ts
+++ src/app/components/batch-management/batch-management.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { BatchManagementRoutingModule } from './batch-management-routing.module';
+import { BatchManagementComponent } from './batch-management.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { LibMaterialModule } from 'dema-movyon-template';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BatchManagementComponent],
   imports: [
     CommonModule,
+    TranslateModule,
+    FlexLayoutModule,
+    LibMaterialModule,
     BatchManagementRoutingModule
   ]
 })
