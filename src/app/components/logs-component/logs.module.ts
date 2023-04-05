6b5c862d9359ff3641ca7cccaf375d7f4a24584b
mmontidema <mirco.monti@benefind.it>
2023-04-05 14:11:09 +0200

    added logs management page

--- src/app/components/logs-component/logs.module.ts
+++ src/app/components/logs-component/logs.module.ts
@@ -4,18 +4,24 @@ import { LibMaterialModule, LibPipesModule } from 'dema-movyon-template';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { TranslateModule } from '@ngx-translate/core';
 import { LogsRoutingModule } from './logs-routing.module';
+import { LogsComponentComponent } from './logs-component.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LogsComponentComponent
+  ],
   imports: [
     CommonModule,
     LibMaterialModule,
     LibPipesModule,
     FlexLayoutModule,
     TranslateModule,
-    LogsRoutingModule
+    LogsRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class LogsModule { }
