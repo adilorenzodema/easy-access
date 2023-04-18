64ca3f1734eb257c1b42c566f2403d8d0f827203
mmontidema <mirco.monti@dema-engineering.it>
2023-04-18 15:50:52 +0200

    fixed holidays

--- src/app/components/holidays-component/holidays-component.component.ts
+++ src/app/components/holidays-component/holidays-component.component.ts
@@ -38,10 +38,10 @@ export class HolidaysComponentComponent implements OnInit {
   ) {
   }
   public dateClass = (date: Date): string[] => {
-    const d: any = new Date(date.setDate(date.getDate() + 1));
+    //const d: any = moment(date).format('yyyy-MM-DD');
     //moment(date).add('day', 1).toDate();
 
-    if (this._findDate(d) !== -1) {
+    if (this._findDate(date) !== -1) {
       return ['selected'];
     }
     return [];
@@ -89,13 +89,14 @@ export class HolidaysComponentComponent implements OnInit {
   public dateChanged(event: MatDatepickerInputEvent<Date>): void {
     if (event.value) {
       /* let date = event.value; */
-      moment(event.value).add(1, 'day');
+      // let date:any = moment(event.value)/*.add(1, 'day')*/;
+      // date = date.format('yyyy-MM-DD');
       /* date = new Date(date.setDate(date.getDate()+1)); */
       const index = this._findDate(event.value);
 
       if (index === -1) {
         /* date = new Date(date.setDate(date.getDate()-1)); */
-        moment(event.value).subtract(1, 'day');
+        // date.subtract(1, 'day');
         this.model.push(event.value);
         this.model.sort((a, b) => this.orderDate(a, b));
       } else {
@@ -113,6 +114,33 @@ export class HolidaysComponentComponent implements OnInit {
     }
   }
 
+  //chiamata per prendere le festività di un anno
+  public getYearHolidays(year: number): void{
+    let dates:Date[] = [];
+    const capodanno = new Date(year + "/01/01");
+    const epifania = new Date(year + "/01/06");
+    const liberazione = new Date(year + "/04/25");
+    const pasqua = this.getEaster(year);
+    const pasquetta = this.getEaster(year);
+    pasquetta.setDate(pasquetta.getDate() +1);
+    const festaLavoro = new Date(year + "/05/01");
+    const festaRepubblica = new Date(year + "/06/02");
+    const assunzione = new Date(year + "/08/15");
+    const ognisanti = new Date(year + "/11/01");
+    const immacolata = new Date(year + "/12/08");
+    const natale = new Date(year + "/12/25");
+    const santoStefano = new Date(year + "/12/26");
+
+    //inserire nelle festività
+    dates = [capodanno, epifania, liberazione, pasqua, pasquetta, festaLavoro, festaRepubblica, assunzione, ognisanti, immacolata, natale, santoStefano];
+
+    for(let d of dates) {
+      if (this._findDate(d) == -1) this.model.push(d);
+    }
+
+    this.model.sort((a, b) => this.orderDate(a, b));
+  }
+
   public remove(date: Date): void {
     /*   const mDate = moment(date).format('yyyy-MM-DD'); //.split('T')[0]
       const mDate1= new Date(mDate);
@@ -124,10 +152,11 @@ export class HolidaysComponentComponent implements OnInit {
   }
 
   private _findDate(date: Date): number {
-    let d: any;
+    let d: string;
+    const myDate:string = moment(date).format('yyyy-MM-DD');
     for (const i in this.model) {
       d = moment(this.model[i]).format('yyyy-MM-DD');
-      if (d === date.toISOString().split('T')[0]) {
+      if (d === myDate) {
         return +i;
       }
     }
@@ -161,6 +190,27 @@ export class HolidaysComponentComponent implements OnInit {
     else if (d1.getTime() < d2.getTime()) return -1;
 
     else return 0; //date uguali
+  }
+
+  //funzioni per get pasqua
+  private getEaster(year: number): any{
+    var C = Math.floor(year/100);
+    var N = year - 19*Math.floor(year/19);
+    var K = Math.floor((C - 17)/25);
+    var I = C - Math.floor(C/4) - Math.floor((C - K)/3) + 19*N + 15;
+    I = I - 30*Math.floor((I/30));
+    I = I - Math.floor(I/28)*(1 - Math.floor(I/28)*Math.floor(29/(I + 1))*Math.floor((21 - N)/11));
+    var J = year + Math.floor(year/4) + I + 2 - C + Math.floor(C/4);
+    J = J - 7*Math.floor(J/7);
+    var L = I - J;
+    var M = 3 + Math.floor((L + 40)/44);
+    var D = L + 28 - 31*Math.floor(M/4);
+
+    return new Date(this.padout(M) + '-' + this.padout(D) + '-' +year);
+  }
+
+  private padout(number): any {
+    return (number < 10) ? '0' + number : number;
   }
 }
 
