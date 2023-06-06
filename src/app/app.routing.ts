91479b5c4449785cdc574cdf745b9255c7754905
gquaresimadema <g.quaresima@dema-engineering.it>
2023-06-06 15:48:12 +0200

    Added CompanyManagement to Interporto

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
         path: 'user-management',
         loadChildren: () => import("dema-users-management").then(m => m.DemaUsersManagementModule),
         canActivate: [AuthGuard]
+      },  {
+        path: 'company-management',
+        loadChildren: () => import("dema-company-management").then(m => m.DemaCompanyManagementModule ),
+        canActivate: [AuthGuard]
       },
       {
         path: 'area-management',
